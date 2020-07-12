ee14ea9508a23a6df662b2a1ea21d0eac4b290bf
GandhiCloudLab <GandhiCloudLab@gmail.com>
2020-07-13 02:29:23 +0530

    deployment file changes

--- build-dockerhub.sh
+++ build-dockerhub.sh
@@ -2,12 +2,10 @@
 
 echo "build Started ...."
 
-# mvn clean package
-
-docker build -f Dockerfile -t gandhicloudlab/classic-wcareweb:latest . 
+docker build -f Dockerfile -t gandhicloudlab/wcareweb:latest . 
 
 # docker login -u gandhicloudlab
 
-docker push gandhicloudlab/classic-wcareweb:latest
+docker push gandhicloudlab/wcareweb:latest
 
 echo "build completed ...."
